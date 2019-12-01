dddfbab0e2a66974dc43b6e6e0ac1fa55b02350a
dsudo <dsudo0624@gmail.com>
2019-12-01 22:13:20 +0900

    Implement threejs step1

---
+++ samples/threejs/app.js
@@ -0,0 +1,32 @@
+const main = (canvas) => {
+  if (!canvas) return;
+
+  // canvas size
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  // scene
+  const scene = new THREE.Scene();
+
+  // mesh
+  const box = new THREE.Mesh(
+    new THREE.BoxGeometry(64, 64, 64),
+    new THREE.MeshNormalMaterial({ color: 0x6699FF })
+  );
+  box.position.set(0, 0, 0);
+  scene.add(box);
+
+  // camera
+  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+  camera.position.set(200, 100, 300);
+  camera.lookAt(scene.position);
+
+  // renderer
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setSize(width, height);
+  renderer.setClearColor(0xd0f0d0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  // render
+  renderer.render(scene, camera);
+};
