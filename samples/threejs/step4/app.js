5d3dfab90c817b4b78111cb7ed43820aeb2c91d7
dsudo <dsudo0624@gmail.com>
2020-02-29 18:04:03 +0900

    Add threejs/step4

---
+++ samples/threejs/step4/app.js
@@ -0,0 +1,53 @@
+const main = (canvas) => {
+  if (!canvas) return;
+
+  // 1. canvas size
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  // 2. scene
+  const scene = new THREE.Scene();
+
+  // 3. mesh (box)
+  const box = new THREE.Mesh(
+    new THREE.BoxGeometry(64, 64, 64),
+    new THREE.MeshNormalMaterial()
+  );
+  box.position.set(0, 0, 0);
+  scene.add(box);
+
+  // 4. camera
+  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+  camera.position.set(200, 100, 300);
+  camera.lookAt(box.position);
+
+  // 4-2. camera controls
+  var controls = new THREE.OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+
+  // 5. renderer
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
+  renderer.setSize(width, height);
+  renderer.setClearColor(0xffffff, 0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  // 6. animate
+  const animate = () => {
+    // next frame
+    requestAnimationFrame(animate);
+
+    // rotate
+    const sec = Date.now() / 1000;
+    box.rotation.x = sec * (Math.PI / 4);
+    box.rotation.y = sec * (Math.PI / 4);
+    box.rotation.z = sec * (Math.PI / 4);
+
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    controls.update();
+
+    // render
+    renderer.render(scene, camera);
+  };
+
+  animate();
+};
