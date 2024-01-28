90fa3056cc40be3309968f4f89b3dc1056a6fb0f
dsudo <24204273+DaisukeSudo@users.noreply.github.com>
2024-01-28 17:25:47 +0900

    gen/2024-01-28

---
+++ gen/2024-01-28/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  AOS.init({
+      duration: 1200, // values from 0 to 3000, with step 50ms
+      once: true, // whether animation should happen only once - while scrolling down
+  });
+});
